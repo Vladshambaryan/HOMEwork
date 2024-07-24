ad3a29c3a920b6b0194011226d7742d6ec322b8d
PC <skachat30913@gmail.com>
2024-07-24 22:38:03 +0400

    update

---
+++ Lesson_16!!!/2.py
@@ -0,0 +1,11 @@
+import collections
+
+with open('shops.txt') as shops_file:
+    shops = list(map(lambda x: x.replace('\n', ''), shops_file.readlines()))
+
+city_shops = {}
+for line in shops:
+    shop, city = line.split(':')
+    city_shops[city] = shop
+
+print(city_shops)
